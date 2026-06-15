fdbfe8b4d51c043438f80a49638fa969ba6398a2
shps100606-creator <shps100606@gmail.com>
2026-06-15 19:37:56 +0800

    feat: add post detail page

---
+++ web/pages/posts/[...slug].vue
@@ -0,0 +1,18 @@
+<template>
+  <main class="article">
+    <template v-if="post">
+      <h1>{{ post.title }}</h1>
+      <p class="meta">{{ post.date }} · {{ post.author || 'moti' }}</p>
+      <div class="article-body">
+        <ContentRenderer :value="post" />
+      </div>
+    </template>
+  </main>
+</template>
+
+<script setup>
+const route = useRoute()
+const { data: post } = await useAsyncData(route.path, () =>
+  queryContent(route.path).findOne()
+)
+</script>
