ad55c350842e2cc1fed15b1e77a62091c77b769e
shps100606-creator <shps100606@gmail.com>
2026-06-21 19:57:57 +0800

    feat(web): add GiscusComments.vue component for article comments (WN2-1)

---
+++ web/components/GiscusComments.vue
@@ -0,0 +1,30 @@
+<template>
+  <div ref="container" class="giscus"></div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const container = ref(null)
+
+onMounted(() => {
+  const script = document.createElement('script')
+  script.src = 'https://giscus.app/client.js'
+  script.setAttribute('data-repo', 'shps100606-creator/moticore')
+  script.setAttribute('data-repo-id', 'PLACEHOLDER_REPO_ID')
+  script.setAttribute('data-category', 'PLACEHOLDER_CATEGORY')
+  script.setAttribute('data-category-id', 'PLACEHOLDER_CATEGORY_ID')
+  script.setAttribute('data-mapping', 'pathname')
+  script.setAttribute('data-strict', '0')
+  script.setAttribute('data-reactions-enabled', '1')
+  script.setAttribute('data-emit-metadata', '0')
+  script.setAttribute('data-input-position', 'bottom')
+  script.setAttribute('data-theme', 'light')
+  script.setAttribute('data-lang', 'zh-TW')
+  script.crossOrigin = 'anonymous'
+  script.async = true
+  if (container.value) {
+    container.value.appendChild(script)
+  }
+})
+</script>
