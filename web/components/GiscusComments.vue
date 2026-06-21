d8106352a9114ffc7a450e2e16c4767a0c6d8d29
shps100606-creator <shps100606@gmail.com>
2026-06-21 22:22:28 +0800

    feat(web): fill in Giscus repo-id and category-id (WN2-1 complete)

--- web/components/GiscusComments.vue
+++ web/components/GiscusComments.vue
@@ -11,9 +11,9 @@ onMounted(() => {
   const script = document.createElement('script')
   script.src = 'https://giscus.app/client.js'
   script.setAttribute('data-repo', 'shps100606-creator/moticore')
-  script.setAttribute('data-repo-id', 'PLACEHOLDER_REPO_ID')
-  script.setAttribute('data-category', 'PLACEHOLDER_CATEGORY')
-  script.setAttribute('data-category-id', 'PLACEHOLDER_CATEGORY_ID')
+  script.setAttribute('data-repo-id', 'R_kgDOSZ-Dng')
+  script.setAttribute('data-category', 'Announcements')
+  script.setAttribute('data-category-id', 'DIC_kwDOSZ-Dns4C_max')
   script.setAttribute('data-mapping', 'pathname')
   script.setAttribute('data-strict', '0')
   script.setAttribute('data-reactions-enabled', '1')
